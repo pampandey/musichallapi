3d740161accb7f950e6afaebaf98f89e6d12e093
Pramila Pandey <pam.pandey@Pramilas-MacBook-Air.local>
2022-09-10 15:19:34 -0400

    musichallapi3

--- functions/src/appointments.js
+++ functions/src/appointments.js
@@ -1,9 +1,11 @@
 import dbConnect from "./dbConnect.js";
+import firebase from "firebase-admin"
 
 export async function getAppointments(req, res) {
   const db = dbConnect();
   const collection = await db
     .collection("appointments")
+    .orderBy("timestamp","desc")
     .get()
     .catch((err) => res.status(500).send(err));
   const appointments = collection.docs.map((doc) => {
@@ -15,7 +17,8 @@ export async function getAppointments(req, res) {
 }
 
 export async function createAppointment(req, res) {
-  const newAppointment = req.body;
+  let newAppointment = req.body;
+  newAppointment.timestamp = firebase.firestore.FieldValue.serverTimestamp()
   if (!newAppointment) {
     res.status(400).send({ success: false, message: "Invalid request" });
     return;
